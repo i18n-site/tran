0103054e072caefeb873450d2b0b660d93140189
i18n <i18n.site@gmail.com>
2025-07-09 15:28:19 +0800

    ♨

--- tran/src/lib.rs
+++ tran/src/lib.rs
@@ -1,6 +1,11 @@
 use aok::{OK, Void};
 use tran_api::Job;
 
+pub trait Traner {
+  fn parse(&mut self) -> Result<String>;
+  fn restore(&mut self) -> Result<String>;
+}
+
 pub fn tran(job: Job) -> Void {
   /*
   缓存中保存
